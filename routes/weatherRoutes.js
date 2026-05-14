77bca99a23d10c41bf3bf2f0a81b7a5ef72cbfb7
bhaikattisakshi <bhaikattisakshi@gmail.com>
2026-05-14 23:04:11 +0530

    Fixed Merge Conflicts and Connected weather API

--- routes/weatherRoutes.js
+++ routes/weatherRoutes.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require("express");
 
 const router = express.Router();
@@ -10,20 +9,3 @@ const {
 router.get("/:city", getWeather);
 
 module.exports = router;
-=======
-const express = require("express");
-
-const router = express.Router();
-
-router.get("/:city", (req, res) => {
-
-    const city = req.params.city;
-
-    res.json({
-        city: city,
-        message: "Weather route working"
-    });
-});
-
-module.exports = router;
->>>>>>> 4132f22335f5aac8eef3e4d2ad886e334d0bee1b
