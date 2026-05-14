f1454e4a8d044a0b94066ea0bea524be4bffd92f
bhaikattisakshi <bhaikattisakshi@gmail.com>
2026-05-14 21:48:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
+require("dotenv").config();
+
 const express = require("express");
 
 const app = express();
+
+const weatherRoutes = require("./routes/weatherRoutes");
+
+app.use("/api/weather", weatherRoutes);
 
 app.get("/", (req, res) => {
     res.send("Weather API Running");
