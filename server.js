d33d3c687f43964707b8ad90de4606c757690ea7
bhaikattisakshi <bhaikattisakshi@gmail.com>
2026-05-17 15:56:34 +0530

    Added rate limiting

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
-
 require("dotenv").config();
 
 const express = require("express");
 
+const rateLimit = require("express-rate-limit");
+
 const app = express();
 
 const weatherRoutes = require("./routes/weatherRoutes");
+
+
+// Rate Limiter
+const limiter = rateLimit({
+
+    windowMs: 15 * 60 * 1000,
+
+    max: 100,
+
+    message: "Too many requests, please try again later."
+
+});
+
+app.use(limiter);
 
 app.use("/api/weather", weatherRoutes);
 
